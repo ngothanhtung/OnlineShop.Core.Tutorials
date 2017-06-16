099d8c494ae90c2dc90f518dbf5ad0f425312367
Ngo Thanh Tung <tungnt@softech.vn>
2017-06-16 21:19:01 +0700

    Add project files.

---
+++ OnlineShop.Core.Tutorials/Data/OnlineShopContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Core.Tutorials.Data;
+
+namespace OnlineShop.Core.Tutorials.Data
+{
+    public class OnlineShopContext : DbContext
+    {
+        public OnlineShopContext (DbContextOptions<OnlineShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
---
+++ OnlineShop.Core.Tutorials/Data/Supplier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+
+namespace OnlineShop.Core.Tutorials.Data
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+    }
+}
